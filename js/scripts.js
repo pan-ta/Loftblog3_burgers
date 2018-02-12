0455de33ae7f14f4c75c282f4f83245401632b74
Tatiana Shimbereva <pan-ta@yandex.ru>
2018-02-13 01:20:11 +0300

    acco never dies

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
 window.onload = function(){
+
+    //////////////////////
+    // hamburger
+
     var hamburgerButton = document.querySelector(".hamburger__button");
     var hamburgerOverlay = document.querySelector(".hamburger__overlay");
     console.log(hamburgerButton);
@@ -6,6 +10,27 @@ window.onload = function(){
     hamburgerButton.addEventListener("click", function(e){
         e.preventDefault();
         hamburgerOverlay.style.display = "initial";
-
     })
+
+    //////////////////////
+    // menu-acco
+
+    // var menuAccoTrigger = document.querySelector(".menu__acco-trigger");
+    // var menuAccoItemContainer = document.querySelector(".menu__acco-item");
+
+    // menuAccoTrigger.addEventListener("click", function(e){
+    //     e.preventDefault();
+    //     menuAccoItemContainer.classList.toggle(".menu__acco-item_active");
+    // })
+
+    var menuAccoTrigger = document.querySelector("#trigger1");
+    var menuAccoItemContainer = document.querySelector("#item1");
+
+    menuAccoTrigger.addEventListener("click", function(e){
+        e.preventDefault();
+        menuAccoItemContainer.classList.remove(".menu__acco-item_active");
+    })
+
 }
+
+
